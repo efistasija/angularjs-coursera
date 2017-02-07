5e6c4700b5b1c58002cce8cf902d6a4e9fe4460a
Ana <efistasija@gmail.com>
2017-02-07 16:31:34 +0100

    module 3 submission code

---
+++ module3_submission/app.js
@@ -0,0 +1,113 @@
+  (
+    function(){
+      'use strict';
+
+      angular.module('NarrowItDownApp',[])
+      .controller('NarrowItDownController', NarrowItDownController)
+      .service('MenuSearchService', MenuSearchService)
+      .constant('MenuItemsURL', "http://davids-restaurant.herokuapp.com")
+      .directive("foundItems", FoundItemsDirective);
+
+      function FoundItemsDirective(){
+        var ddo = {
+          templateUrl: "foundItems.html",
+          scope: {
+            found: "<",
+            onRemove: "&"
+          },
+          controller: FoundItemsDirectiveController,
+          bindToController: true,
+          controllerAs: "list",
+          link: FoundItemsDirectiveLink
+        };
+
+        return ddo;
+      };
+
+      //the directive link
+      function FoundItemsDirectiveLink(scope, element, attrs, controller){
+        scope.$watch('list.chechIfEmptyList()', function(newValue, oldValue){
+          if(newValue === true){
+            console.log("empty");
+            displayMenuMessage();
+          }
+          else{
+            console.log("filled");
+
+            hideMenuMessage();
+          }
+        });
+
+        function displayMenuMessage(){
+          var messageElem = element.find("div.msg");
+          messageElem.slideDown(300);
+        }
+
+        function hideMenuMessage(){
+          var messageElem = element.find("div.msg");
+          messageElem.slideUp(300);
+        }
+      }
+
+      //the directive controller
+      function FoundItemsDirectiveController(){
+        var list = this;
+
+        list.chechIfEmptyList = function(){
+          return (((list.found !==undefined) && (list.found.length === 0)) ?  true : false);
+        };
+      }
+
+      //main controller
+      NarrowItDownController.$inject = ['MenuSearchService'];
+      function NarrowItDownController(MenuSearchService){
+        var narrowCtrl = this;
+
+        narrowCtrl.searchTerm = "";
+
+        narrowCtrl.narrowDownMenu = function(){
+          var promise = MenuSearchService.getMatchedMenuItems(narrowCtrl.searchTerm);
+
+          promise.then(function(result){
+            narrowCtrl.found = result;
+
+              console.log(narrowCtrl.found.length)
+
+          })
+        };
+
+        narrowCtrl.removeMenuItem = function(index){
+          narrowCtrl.found.splice(index,1);
+        }
+      };
+
+      //service
+      MenuSearchService.$inject = ['$http', 'MenuItemsURL'];
+      function MenuSearchService($http, MenuItemsURL) {
+        var service = this;
+
+        service.getMatchedMenuItems = function(searchTerm){
+
+          return $http({
+            method: 'GET',
+            url: (MenuItemsURL + '/menu_items.json')
+          }).then(function(result){
+            var foundItems = [];
+            if(searchTerm){
+              var items = result.data.menu_items;
+
+              for(var i=0; i< items.length; i++){
+                  if(items[i]["description"].toLowerCase().indexOf(searchTerm) !== -1){
+                    foundItems.push(items[i]);
+                  }
+                }
+              }//end if
+
+                return foundItems;
+              }).catch(function(error){});
+
+        }
+      };
+
+    }
+  )();
